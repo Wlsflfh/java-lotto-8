86e8b13dedd0ccf915d0d71b8074b7cd1d8d371e
Jinriro <riro12.hanyang.ac.kr>
2025-10-28 19:37:39 +0900

    test: Money 객체에 대한 테스트 코드 작성

---
+++ src/test/java/lotto/domain/model/MoneyTest.java
@@ -0,0 +1,78 @@
+package lotto.domain.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MoneyTest {
+
+    @Test
+    @DisplayName("구입 금액에 해당하는 만큼 로또를 발행하는지 확인한다.")
+    void calculateTicketCountTest() {
+        // given
+        String userInput = "8000";
+        Money money = new Money(userInput);
+
+        // when
+        int ticketCount = money.calculateTicketCount();
+
+        // then
+        assertThat(ticketCount).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("구입 금액 입력에서 문자를 입력했을 때 예외를 던지는지 확인한다")
+    void validateNumericTest() {
+        // given
+        String userInput = "천원";
+
+        // when - then
+        assertThatThrownBy(() -> new Money(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 숫자만 가능합니다.");
+    }
+
+    @Test
+    @DisplayName("음수 혹은 0을 입력했을 때 예외를 발생시키는지 확인한다")
+    void validateMinRangeTest() {
+        // given
+        String userInput1 = "-2000";
+        String userInput2 = "0";
+
+        // when - then
+        assertThatThrownBy(() -> new Money(userInput1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 양수만 가능합니다.");
+
+        // when - then
+        assertThatThrownBy(() -> new Money(userInput2))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 양수만 가능합니다.");
+    }
+
+    @Test
+    @DisplayName("최대 금액인 백만원을 넘어가는 입력에 대해 예외를 발생시키는지 확인한다")
+    void validateMaxRangeTest() {
+        // given
+        String userInput = "10000000";
+
+        // when - then
+        assertThatThrownBy(() -> new Money(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 1,000,000원 이하만 가능합니다.");
+    }
+
+    @Test
+    @DisplayName("천원 단위의 숫자를 입력하지 않았을 때 예외를 발생시키는지 확인한다")
+    void validateUnitTest() {
+        // given
+        String userInput = "999";
+
+        // when - then
+        assertThatThrownBy(() -> new Money(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 구입 금액은 1,000원 단위여야 합니다.");
+    }
+}
