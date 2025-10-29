df7064f209d5e2f1d6b61f42f8686d2828694523
Jinriro <riro12.hanyang.ac.kr>
2025-10-29 15:46:53 +0900

    feat: 출력을 위한 당첨 결과 이동할LottoResultDto 구현

--- src/main/java/lotto/service/dto/LottoResultDto.java
+++ src/main/java/lotto/service/dto/LottoResultDto.java
@@ -1,2 +1,24 @@
-package lotto.service;public class LottoResultDto {
+package lotto.service.dto;
+
+import lotto.domain.Prize;
+
+import java.util.Map;
+
+public class LottoResultDto {
+
+    private final Map<Prize, Integer> lottoResult;
+    private final double earningRate;
+
+    public LottoResultDto(Map<Prize, Integer> lottoResult, double earningRate) {
+        this.lottoResult = lottoResult;
+        this.earningRate = earningRate;
+    }
+
+    public Map<Prize, Integer> getLottoResult() {
+        return lottoResult;
+    }
+
+    public double getEarningRate() {
+        return earningRate;
+    }
 }
