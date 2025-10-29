0b6239f9760b490d3c1a219776efd1cf5fa9a36e
Jinriro <riro12.hanyang.ac.kr>
2025-10-30 00:43:49 +0900

    test: LottoServiceTest 클래스 test 케이스 작성

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,4 +1,63 @@
-import static org.junit.jupiter.api.Assertions.*;
+package lotto.service;
+
+import lotto.domain.*;
+import lotto.domain.generator.LottoGenerator;
+import lotto.domain.generator.RandomNumberGenerator;
+import lotto.service.dto.LottoResultDto;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class LottoServiceTest {
-  
+
+    class FixedLottoGenerator implements RandomNumberGenerator {
+
+        @Override
+        public List<Integer> generate() {
+            return List.of(1, 2, 3, 4, 5, 6);
+        }
+    }
+
+    @Test
+    @DisplayName("로또 결과를 올바르게 계산하는지 확인한다")
+    void calculateLottoResultTest() {
+        // given
+        Money money = new Money(1000);
+        LottoMachine lottoMachine = new LottoMachine(money.calculateTicketCount(), new FixedLottoGenerator());
+        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), new LottoNumber(8));
+
+        // when
+        LottoService lottoService = new LottoService(lottoMachine, winningLotto);
+
+        // then
+        LottoResultDto lottoResultDto = lottoService.calculateLottoResult(money);
+        Map<Prize, Integer> lottoResult = lottoResultDto.getLottoResult();
+
+        assertEquals(1, lottoResult.get(Prize.PLACE_OF_1ST));
+        for (Prize prize : Prize.values()) {
+            if (prize != Prize.PLACE_OF_1ST) {
+                assertEquals(0, lottoResult.get(prize));
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("로또 결과에 따른 수익률을 올바르게 계산하는지 확인한다")
+    void calculateEarningRateTest() {
+        // given
+        Money money = new Money(1000);
+        LottoMachine lottoMachine = new LottoMachine(money.calculateTicketCount(), new FixedLottoGenerator());
+        WinningLotto winningLotto = new WinningLotto(new Lotto(List.of(1, 2, 3, 4, 5, 6)), new LottoNumber(8));
+
+        // when
+        LottoService lottoService = new LottoService(lottoMachine, winningLotto);
+
+        // then
+        LottoResultDto lottoResultDto = lottoService.calculateLottoResult(money);
+        assertEquals(lottoResultDto.getEarningRate(), 200000000.0);
+    }
 }
