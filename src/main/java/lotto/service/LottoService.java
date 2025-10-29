2c6ecfb68674f1ff12839a0bf7d80439a3db07f2
Jinriro <riro12.hanyang.ac.kr>
2025-10-29 15:44:20 +0900

    feat: LottoService 구현

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,2 +1,52 @@
-package lotto.service;public class LottoService {
+package lotto.service;
+
+import lotto.domain.Money;
+import lotto.domain.LottoMachine;
+import lotto.domain.MatchResult;
+import lotto.domain.Prize;
+import lotto.domain.WinningLotto;
+import lotto.service.dto.LottoResultDto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LottoService {
+
+    private final LottoMachine lottoMachine;
+    private final WinningLotto winningLotto;
+
+    public LottoService(LottoMachine lottoMachine, WinningLotto winningLotto) {
+        this.lottoMachine = lottoMachine;
+        this.winningLotto = winningLotto;
+    }
+
+    public LottoResultDto calculateLottoResult(Money money) {
+        Map<Prize, Integer> lottoResult = initLottoResult();
+        double totalPrizeMoney = 0;
+
+        List<MatchResult> lottoMatchCount = lottoMachine.calculateMatchCount(winningLotto);
+
+        for (MatchResult matchResult : lottoMatchCount) {
+            Prize prize = Prize.from(matchResult);
+            lottoResult.merge(prize, 1, Integer::sum);
+            totalPrizeMoney += prize.getPrizeMoney();
+        }
+
+        return new LottoResultDto(lottoResult, calculateEarningRate(totalPrizeMoney, money));
+    }
+
+    private Map<Prize, Integer> initLottoResult() {
+        Map<Prize, Integer> lottoResult = new HashMap<>();
+
+        for (Prize prize : Prize.values()) {
+            lottoResult.put(prize, 0);
+        }
+
+        return lottoResult;
+    }
+
+    private double calculateEarningRate(double totalPrizeMoney, Money money) {
+        return totalPrizeMoney / money.getMoney() * 100;
+    }
 }
