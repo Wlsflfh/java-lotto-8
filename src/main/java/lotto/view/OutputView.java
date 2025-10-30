4e991a631d658572e9a2c0e59f2a24b9d0dc68cc
Jinriro <riro12.hanyang.ac.kr>
2025-10-30 15:00:24 +0900

    refactor: 출력 메세지 상수화

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -8,16 +8,19 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
-import static lotto.domain.Prize.*;
-
 public class OutputView {
+
+    private static final String TICKET_COUNT_HEADER_MESSAGE = "\n%d개를 구매했습니다.\n";
+    private static final String LOTTO_RESULT_HEADER_MESSAGE = "\n당첨 통계\n---";
+    private static final String LOTTO_RESULT_FORMAT = "%s - %d개\n";
+    private static final String EARNING_RATE_MESSAGE = "총 수익률은 %.1f%%입니다.";
 
     public static void showErrorMessage(String errorMessage) {
         System.out.println(errorMessage);
     }
 
     public void printLottoTickets(int ticketCount, List<List<LottoNumber>> lottoTickets) {
-        System.out.printf("\n%d개를 구매했습니다.\n", ticketCount);
+        System.out.printf(TICKET_COUNT_HEADER_MESSAGE, ticketCount);
 
         for (List<LottoNumber> lottoTicket : lottoTickets) {
             System.out.println(lottoTicket);
@@ -28,22 +31,13 @@ public class OutputView {
         Map<Prize, Integer> sortedResult = new EnumMap<>(Prize.class);
         sortedResult.putAll(lottoResultDto.getLottoResult());
 
-        System.out.println("\n당첨 통계\n---");
+        System.out.println(LOTTO_RESULT_HEADER_MESSAGE);
         for (Map.Entry<Prize, Integer> prizeIntegerEntry : sortedResult.entrySet()) {
-            if (fromPrize(prizeIntegerEntry.getKey()).equals("NONE")) continue;
+            if (PrizeView.from(prizeIntegerEntry.getKey()).equals(PrizeView.NONE)) continue;
 
-            System.out.printf("%s - %d개\n", fromPrize(prizeIntegerEntry.getKey()), prizeIntegerEntry.getValue());
+            System.out.printf(LOTTO_RESULT_FORMAT, PrizeView.from(prizeIntegerEntry.getKey()).getMessage(), prizeIntegerEntry.getValue());
         }
 
-        System.out.printf("총 수익률은 %.1f%%입니다.", lottoResultDto.getEarningRate());
-    }
-
-    private String fromPrize(Prize prize) {
-        if (prize == PLACE_OF_5ST) return "3개 일치 (5,000원)";
-        if (prize == PLACE_OF_4ST) return "4개 일치 (50,000원)";
-        if (prize == PLACE_OF_3ST) return "5개 일치 (1,500,000원)";
-        if (prize == PLACE_OF_2ST) return "5개 일치, 보너스 볼 일치 (30,000,000원)";
-        if (prize == PLACE_OF_1ST) return "6개 일치 (2,000,000,000원)";
-        return "NONE";
+        System.out.printf(EARNING_RATE_MESSAGE, lottoResultDto.getEarningRate());
     }
 }
