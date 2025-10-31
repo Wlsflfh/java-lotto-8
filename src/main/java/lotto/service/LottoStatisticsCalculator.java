fd990f056b1fea349e1ade7dcc4fea7ef17fd5b7
Jinriro <riro12.hanyang.ac.kr>
2025-10-31 18:38:38 +0900

    refactor: 네이밍 수정 및 리팩토링

--- src/main/java/lotto/service/LottoStatisticsCalculator.java
+++ src/main/java/lotto/service/LottoStatisticsCalculator.java
@@ -1,42 +1,40 @@
 package lotto.service;
 
 import lotto.domain.*;
-import lotto.domain.generator.LottoMachine;
-import lotto.service.dto.LottoResultDto;
+import lotto.service.dto.LottoStatisticsDto;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class LottoService {
+public class LottoStatisticsCalculator {
 
-    private static final int PERCENT = 100;
     private static final int INIT_COUNT = 0;
 
     private final LottoTickets lottoTickets;
     private final WinningLotto winningLotto;
 
-    public LottoService(LottoTickets lottoTickets, WinningLotto winningLotto) {
+    public LottoStatisticsCalculator(LottoTickets lottoTickets, WinningLotto winningLotto) {
         this.lottoTickets = lottoTickets;
         this.winningLotto = winningLotto;
     }
 
-    public LottoResultDto calculateLottoResult(Money money) {
-        Map<Prize, Integer> lottoResult = initLottoResult();
-        double totalPrizeMoney = 0;
+    public LottoStatisticsDto calculateLottoStatistics(Money money) {
+        Map<Prize, Integer> lottoStatistics = createEmptyLottoStatistics();
+        PrizeStatistics prizeStatistics = new PrizeStatistics();
 
-        List<MatchResult> lottoMatchCount = lottoTickets.calculateMatchCount(winningLotto);
+        List<MatchResult> lottoMatchCount = lottoTickets.matchAllWith(winningLotto);
 
         for (MatchResult matchResult : lottoMatchCount) {
             Prize prize = Prize.from(matchResult);
-            lottoResult.merge(prize, 1, Integer::sum);
-            totalPrizeMoney += prize.getPrizeMoney();
+            lottoStatistics.merge(prize, 1, Integer::sum);
+            prizeStatistics.accumulate(prize);
         }
 
-        return new LottoResultDto(lottoResult, calculateEarningRate(totalPrizeMoney, money));
+        return new LottoStatisticsDto(lottoStatistics, prizeStatistics.getEarningRate(money));
     }
 
-    private Map<Prize, Integer> initLottoResult() {
+    private Map<Prize, Integer> createEmptyLottoStatistics() {
         Map<Prize, Integer> lottoResult = new HashMap<>();
 
         for (Prize prize : Prize.values()) {
@@ -44,9 +42,5 @@ public class LottoService {
         }
 
         return lottoResult;
-    }
-
-    private double calculateEarningRate(double totalPrizeMoney, Money money) {
-        return totalPrizeMoney / money.getMoney() * PERCENT;
     }
 }
