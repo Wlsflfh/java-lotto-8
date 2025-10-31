96d7f31c2cf7dab58dd952694474e4e17edea0de
Jinriro <riro12.hanyang.ac.kr>
2025-10-31 18:33:21 +0900

    refactor: 확장성을 고려해 상금 객체 포장

--- src/main/java/lotto/domain/TotalPrizeMoney.java
+++ src/main/java/lotto/domain/TotalPrizeMoney.java
@@ -1,4 +1,20 @@
 package lotto.domain;
 
-public class TotalPrize {
+public class TotalPrizeMoney {
+
+    private static final int PERCENT = 100;
+
+    private double amount;
+
+    public TotalPrizeMoney() {
+        this.amount = 0;
+    }
+
+    public double calculateEarningRate(Money money) {
+        return (amount / money.getMoney()) * PERCENT;
+    }
+
+    public void accumulate(Prize prize) {
+        amount += prize.getPrizeMoney();
+    }
 }
