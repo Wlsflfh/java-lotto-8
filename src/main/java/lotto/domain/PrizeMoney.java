189eee320279ef248b379e2cd85b1874be602bce
Jinriro <riro12.hanyang.ac.kr>
2025-10-31 20:23:55 +0900

    refactor: 매직 넘버 상수화

--- src/main/java/lotto/domain/PrizeMoney.java
+++ src/main/java/lotto/domain/PrizeMoney.java
@@ -1,24 +1,25 @@
 package lotto.domain;
 
-public class PrizeStatistics {
+public class PrizeMoney {
 
     private static final int PERCENT = 100;
+    private static final int INTI_AMOUNT = 0;
 
     private double amount;
 
-    public PrizeStatistics() {
-        this.amount = 0;
+    public PrizeMoney() {
+        this.amount = INTI_AMOUNT;
     }
 
     public void accumulate(Prize prize) {
         amount += prize.getPrizeMoney();
     }
 
-    public double getEarningRate(Money money) {
-        return calculateEarningRate(money);
+    public double getEarningRate(PurchaseAmount purchaseAmount) {
+        return calculateEarningRate(purchaseAmount);
     }
 
-    private double calculateEarningRate(Money money) {
-        return (amount / money.getMoney()) * PERCENT;
+    private double calculateEarningRate(PurchaseAmount purchaseAmount) {
+        return (amount / purchaseAmount.getAmount()) * PERCENT;
     }
 }
