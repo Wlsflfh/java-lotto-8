379e1ebfaea0a615020394535594a9d3364ab957
Jinriro <riro12.hanyang.ac.kr>
2025-10-31 18:31:06 +0900

    refactor: LottoMachine에서 발행한 로또들 포장하는 객체 생성

--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,2 +1,32 @@
-package lotto.domain;public class LottoTicker {
+package lotto.domain;
+
+import java.util.List;
+
+public class LottoTicket {
+
+    private final List<LottoNumber> numbers;
+
+    public LottoTicket(List<LottoNumber> numbers) {
+        this.numbers = numbers;
+    }
+
+    public int countMatching(Lotto winningNumbers) {
+        int count = 0;
+
+        for (LottoNumber lottoNumber : numbers) {
+            if (winningNumbers.contains(lottoNumber)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public boolean contains(LottoNumber bonusNumber) {
+        return numbers.contains(bonusNumber);
+    }
+
+    public List<LottoNumber> getNumbers() {
+        return numbers;
+    }
 }
--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -1,4 +1,29 @@
 package lotto.domain;
 
-public class LottoTickers {
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoTickets {
+
+    private final List<LottoTicket> tickets;
+
+    public LottoTickets(List<LottoTicket> tickets) {
+        this.tickets = List.copyOf(tickets);
+    }
+
+    public List<MatchResult> matchAllWith(WinningLotto winningLotto) {
+        List<MatchResult> lottoMatchCount = new ArrayList<>();
+
+        for (LottoTicket lottoTicket : tickets) {
+            int count = winningLotto.countMatchingNumbers(lottoTicket);
+            boolean isBonus = winningLotto.containsBonusNumber(lottoTicket);
+            lottoMatchCount.add(new MatchResult(count, isBonus));
+        }
+
+        return lottoMatchCount;
+    }
+
+    public List<LottoTicket> getTickets() {
+        return tickets;
+    }
 }
